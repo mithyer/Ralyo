1f8685e2f33d54b072b660bce8d909d3444c2f3b
maorui <brightmar@gmail.com>
2019-01-18 01:16:22 +0800

    bug fix

--- src/Extension/CGImage+Atalas.swift
+++ src/Extension/CGImage+Atalas.swift
@@ -8,9 +8,8 @@
 
 import Foundation
 import CoreImage
-import UIKit
 
-extension CIImage {
+extension CGImage {
     
     public class AtalasUnarchiver {
         
@@ -40,7 +39,7 @@ extension CIImage {
         
         private lazy var imageDic = [String: CGImage]()
         
-        public init(fileName: String) {
+        public init(fileName: String, scale: CGFloat) {
             
             let path = Bundle.main.path(forResource: "\(fileName).atlasc/\(fileName)", ofType: "plist")!
             let atalsData = try! Data.init(contentsOf: URL.init(fileURLWithPath: path), options: [.mappedIfSafe, .uncached])
@@ -53,8 +52,7 @@ extension CIImage {
                 let imgExtension = imgPath.pathExtension
                 let path = Bundle.main.path(forResource: "\(fileName).atlasc/\(imgFile)", ofType: imgExtension)!
                 let data = try! Data.init(contentsOf: URL.init(fileURLWithPath: path), options: [.mappedIfSafe, .uncached])
-                let ciImg = CIImage.init(data: data, options: [CIImageOption.colorSpace : CGColorSpaceCreateDeviceRGB()])!
-                ciImg.clampedToExtent()
+                let imgSource = CIImage.init(data: data)!
 
                 let context = CIContext()
                 let subimages = group.subimages!
@@ -62,21 +60,28 @@ extension CIImage {
                     
                     let sourceSize = NSCoder.cgSize(for: info.spriteSourceSize)
                     let offset = NSCoder.cgPoint(for: info.spriteOffset)
-                    let textureRect = NSCoder.cgRect(for: info.textureRect)
+                    var textureRect = NSCoder.cgRect(for: info.textureRect)
+                    textureRect.origin.y = imgSource.extent.height - textureRect.maxY
                     let rotated = info.textureRotated!
                     let name = (info.name as NSString).deletingPathExtension
+                    let img: CIImage = imgSource.cropped(to: textureRect)
+                    var cgImg = context.createCGImage(img, from: img.extent)!
                     
-                    var img = ciImg.cropped(to: textureRect)
                     if rotated {
-                        img = img.oriented(forExifOrientation: Int32(CGImagePropertyOrientation.left.rawValue))
+                        let context = CGContext.init(data: nil, width: Int(textureRect.height), height: Int(textureRect.width), bitsPerComponent: 8, bytesPerRow: 0, space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue)!
+                        context.translateBy(x: textureRect.height/2, y: textureRect.width/2)
+                        context.rotate(by: CGFloat.pi/2)
+                        context.draw(cgImg, in: CGRect.init(x: -textureRect.width/2, y: -textureRect.height/2, width: textureRect.width, height: textureRect.height))
+                        cgImg = context.makeImage()!
                     }
-                    if offset.x > 0 || offset.y > 0 {
-                        img = img.cropped(to: CGRect.init(x: -offset.x, y: -offset.y, width: sourceSize.width, height: sourceSize.height))
-                    }
-                    self.imageDic[name] = context.createCGImage(ciImg, from: img.extent)
+
+                    let context = CGContext.init(data: nil, width: Int(sourceSize.width * scale), height: Int(sourceSize.height * scale), bitsPerComponent: 8, bytesPerRow: 0, space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue)!
+                    context.draw(cgImg, in: CGRect.init(x: offset.x * scale, y: offset.y * scale, width: (rotated ? textureRect.height : textureRect.width) * scale, height: (rotated ? textureRect.width : textureRect.height) * scale))
+                    cgImg = context.makeImage()!
+ 
+                    self.imageDic[name] = cgImg
                 }
             }
-            print(self.imageDic.keys)
         }
         
         public func image(forName name: String) -> CGImage? {
